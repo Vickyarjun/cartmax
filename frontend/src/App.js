d7907bc293476a692154bb38d279b3847fa09969
Vignesh <vickyarjun002@gmail.com>
2022-07-28 23:34:35 -0400

    cartmax-initial-commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">cartmax</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
+
+// 31 min
